2f0468edd5ef141ef48d372ba4ceb45315846374
Kostas Tziridis <kostoulhs@users.noreply.github.com>
2014-08-10 15:51:05 +0300

    another config(Cyanogen)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_BASE            := 0x80200000
 BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE        := 2048
 TARGET_KERNEL_SOURCE         := kernel/samsung/msm8930-common
-TARGET_KERNEL_CONFIG         := cyanogenmod/msm8930_loganre_defconfig
+TARGET_KERNEL_CONFIG         := cyanogen_loganre_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8930_loganre_eur_lte_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 
