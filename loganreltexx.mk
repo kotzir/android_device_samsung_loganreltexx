fffa23ee4d5792f313b976d77a844d3cae4a4ce1
kostoulhs <MrKostoulhs@gmail.com>
2014-09-10 01:55:49 +0300

    update NFC

--- loganreltexx.mk
+++ loganreltexx.mk
@@ -54,7 +54,8 @@ PRODUCT_COPY_FILES += \
 
 # NFC configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
