a45ad405a8fc982529e73210d7581f2b94c3a7de
kostoulhs <MrKostoulhs@gmail.com>
2014-09-29 17:34:58 +0300

    update build fingerprint from latest stock rom

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/loganreltexx/full_loganreltexx.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=loganreltexx TARGET_DEVICE=loganrelte BUILD_FINGERPRINT="samsung/loganreltexx/loganrelte:4.2.2/JDQ39/S7275RXXUANF2:user/release-keys" PRIVATE_BUILD_DESC="loganreltexx-user 4.2.2 JDQ39 S7275RXXUANF2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=loganreltexx TARGET_DEVICE=loganrelte BUILD_FINGERPRINT="samsung/loganreltexx/loganrelte:4.2.2/JDQ39/S7275RXXUANG1:user/release-keys" PRIVATE_BUILD_DESC="loganreltexx-user 4.2.2 JDQ39 S7275RXXUANG1 release-keys"
 
 PRODUCT_DEVICE := loganreltexx
 PRODUCT_NAME := cm_loganreltexx
