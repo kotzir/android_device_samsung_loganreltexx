2d3a68b9df4bc0563faab66eec15287eaeb5435f
kostoulhs <MrKostoulhs@gmail.com>
2015-01-06 08:53:07 +0200

    system_prop: reorganize * Disable QCOM's HW AAC encoder * Remove non-existent RIL props * Disable LPA * Disable audio offload

--- system_prop.mk
+++ system_prop.mk
@@ -6,39 +6,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.usb.default.config=mtp
 
-# QC Perf
+# RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=/system/lib/libqc-opt.so
-
-# Common build properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product_ship=true \
-    rild.libpath=/system/lib/libsec-ril.so \
-    rild.libargs=-d/dev/smd0 \
-    telephony.lteOnGsmDevice=1 \
-    ro.telephony.default_network=9 \
-    wifi.interface=wlan0 \
-    ro.chipname=MSM8930 \
-    ro.config.ehrpd=true \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.use_se_table_only=1 \
-    persist.radio.fill_eons=1 \
-    persist.radio.prefer_spn=0 \
-    persist.data.netmgrd.qos.enable=false \
-    ro.sf.lcd_density=240 \
-    ro.ril.transmitpower=true \
-    ro.warmboot.capability=1 \
-    ro.qualcomm.cabl=0 \
-    debug.composition.type=c2d \
-    ro.opengles.version=196608 \
-    mm.enable.qcom_parser=33395 \
-    persist.audio.fluence.mode=endfire \
-    persist.audio.vr.enable=false \
-    persist.audio.handset.mic=digital \
-    lpa.decode=true \
     persist.rild.nitz_plmn="" \
     persist.rild.nitz_long_ons_0="" \
     persist.rild.nitz_long_ons_1="" \
@@ -49,37 +18,59 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3="" \
     ril.subscription.types=NV,RUIM \
+    ro.telephony.ril_class=LoganreRIL \
+    persist.radio.add_power_save=1
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qmienabled=true \
     persist.gps.qc_nlp_in_use=1 \
     ro.qc.sdk.izat.premium_enabled=0 \
     ro.qc.sdk.izat.service_mask=0x0 \
-    ro.gps.agps_provider=1 \
-    persist.fuse_sdcard=true \
-    ro.vold.umsdirtyratio=50 \
-    ro.cwm.enable_key_repeat=true \
+    ro.gps.agps_provider=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.gapless.playback.disable=true \
+    audio.offload.disable=1 \
+    lpa.decode=false \
+    mm.enable.qcom_parser=3407871 \
+    mm.enable.smoothstreaming=true \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    persist.audio.lowlatency.rec=false \
+    qcom.hw.aac.encoder=false
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
-    persist.timed.enable=true \
-    persist.audio.lowlatency.rec=false \
-    audio.gapless.playback.disable=true \
-    qcom.hw.aac.encoder=true \
+    ro.sf.lcd_density=240 \
+    ro.opengles.version=196608 \
+    ro.qualcomm.cabl=0
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=MSM8930AB \
+    ro.product_ship=true \
+    ro.warmboot.capability=1 \
+    ro.vold.umsdirtyratio=50 \
     ro.config.max_starting_bg=8 \
-    camera2.portability.force_api=1 \
-    mm.enable.smoothstreaming=true
+    persist.fuse_sdcard=true \
+    persist.timed.enable=true \
+    wifi.interface=wlan0
 
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=LoganreRIL
-
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0 \
-    ro.debuggable=1
-
-# Enable Samsung EMS dial path
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril.config=samsungEMSReq
-
+# Locale
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.locale.language=en \
     ro.product.locale.region=GB
+
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
