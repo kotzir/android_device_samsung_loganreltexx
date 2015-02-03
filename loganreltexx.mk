ef2e6bf8a1ff4f34ae42a06324e0dbd1127c4a47
kostoulhs <MrKostoulhs@gmail.com>
2015-02-03 10:51:29 +0200

    remove init.carrier.rc

--- loganreltexx.mk
+++ loganreltexx.mk
@@ -86,7 +86,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
-    $(LOCAL_PATH)/rootdir/init.carrier.rc:root/init.carrier.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
 # Etc scripts
