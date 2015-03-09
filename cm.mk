107ec8acdde30902a01c8ada5d81ff8591fab782
kostoulhs <MrKostoulhs@gmail.com>
2015-03-09 08:06:35 +0200

    Revert "move custom AAPT config to cm.mk"
    This reverts commit cb2bad0cb6f6b2b94289e34488c8673c7066158b.

--- cm.mk
+++ cm.mk
@@ -6,10 +6,6 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/loganreltexx/full_loganreltexx.mk)
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=loganreltexx TARGET_DEVICE=loganrelte BUILD_FINGERPRINT="samsung/loganreltexx/loganrelte:4.2.2/JDQ39/S7275RXXUANG1:user/release-keys" PRIVATE_BUILD_DESC="loganreltexx-user 4.2.2 JDQ39 S7275RXXUANG1 release-keys"
 
 PRODUCT_DEVICE := loganreltexx
