8f8360b14a1779f2092e6b4ab4612742c0ae9033
Kostas Tziridis <kostoulhs@users.noreply.github.com>
2016-01-31 20:12:17 +0200

    Switch to phone-hdpi dalvik heap configuration

--- loganreltexx.mk
+++ loganreltexx.mk
@@ -136,4 +136,4 @@ $(call inherit-product, device/samsung/loganreltexx/system_prop.mk)
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
 
 # call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-dalvik-heap.mk)
