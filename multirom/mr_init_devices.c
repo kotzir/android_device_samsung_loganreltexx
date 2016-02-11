276813ce2535b37c97396ccc13f4762802a58a1f
kostoulhs <MrKostoulhs@gmail.com>
2016-02-11 12:06:10 +0200

    update mr_init_devices.c

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -10,29 +10,37 @@ const char *mr_init_devices[] =
 	// Framebuffer
 	"/sys/class/graphics/fb0",
 
-	// ADB stuff
-	"/sys/bus/usb",
-	"/sys/class/android_usb/android0/f_adb",
-	"/sys/class/misc/android_adb",
-	"/sys/class/tty/ptmx",
-	
-	// EMMC stuff
 	"/sys/block/mmcblk0",
+	"/sys/devices/platform/msm_sdcc.1*",
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0",
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p13", // boot
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p21", // cache
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p1",  // modem
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p19", // persist
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p20", // system
+	"/sys/devices/platform/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p23", // userdata
 	"/sys/bus/mmc",
 	"/sys/bus/mmc/drivers/mmcblk",
-	"/sys/devices/platform/msm_sdcc.1*",
-	"/sys/module/mmcblk",
 	"/sys/module/mmc_core",
+	"/sys/module/mmcblk",
+
+	// Input stuff
+        "/sys/devices/virtual/misc/uinput",
+        "/sys/devices/virtual/input*",
+	"/sys/devices/platform/sec_keys/input*", // Volume keys
+	"/sys/devices/platform/msm_ssbi.0/pm8921-core/sec_powerkey/input*", // Power key
+
+	// ADB stuff
+        "/sys/devices/virtual/tty/ptmx",
+        "/sys/devices/virtual/misc/android_adb",
+        "/sys/devices/virtual/android_usb/android0/f_adb",
+        "/sys/bus/usb",
 	
 	// MicroSD card
 	"/sys/block/mmcblk1",
 	"/sys/devices/platform/msm_sdcc.2*",
-
-	// Input stuff
-        "/sys/class/misc/uinput",
-	"/sys/devices/virtual/input/input2*", // Touchscreen
-	"/sys/devices/platform/sec_keys/input*", // Volume keys
-	"/sys/devices/platform/msm_ssbi.0/pm8921-core/sec_powerkey/input*", // Power key
 
 	// USB-OTG
 	"/sys/devices/platform/msm_hsusb_host*",
